a20a542c749c3e570b9309a2eb7840f7d280a81b
Henry Pickersgill <htap2@live.co.uk>
2025-08-05 22:29:35 +0100

    restructure main file to header file

--- arduino_7_seg.hpp
+++ arduino_7_seg.hpp
@@ -1,26 +1,79 @@
-#include "consts.hpp"
+/*
+     6
+    ---
+11|     |7
+  |  12 |
+    ---
+10|     |8
+  |     |
+    ---
+     9
+
+  pin 13 is decimal point
+
+  Digit 1 = Arduino pin 2
+  Digit 2 = Arduino pin 3
+  Digit 3 = Arduino pin 4
+  Digit 4 = Arduino pin 5
+
+Seven segment display pins:                 
+-----------------------------------
+|      .   .   .   .   .   .      |
+|      1   2   3   4   5   6      |  
+|      .   .   .   .   .   .      | 
+|      7   8   9  10  11  12      | 
+----------------------------------- 
+
+Mapping
+_______________________________________________________
+Arduino pins: [2, 3, 4, 5, 6, 7, 8,  9, 10, 11, 12, 13]
+Display pins: [6, 3, 2, 7, 5, 1, 9, 11, 12,  4,  8, 10]
+*/
+
+// Arrays of HIGHs and LOWs to define seven-segment numbers
+int zero[8]  = {1, 1, 1, 1, 1, 1, 0};
+int one[8]   = {0, 1, 1, 0, 0, 0, 0};
+int two[8]   = {1, 1, 0, 1, 1, 0, 1};
+int three[8] = {1, 1, 1, 1, 0, 0, 1};
+int four[8]  = {0, 1, 1, 0, 0, 1, 1};
+int five[8]  = {1, 0, 1, 1, 0, 1, 1};
+int six[8]   = {1, 0, 1, 1, 1, 1, 1};
+int seven[8] = {1, 1, 1, 0, 0, 0, 0};
+int eight[8] = {1, 1, 1, 1, 1, 1, 1};
+int nine[8]  = {1, 1, 1, 1, 0, 1, 1};
+
+// Pins controlling the four digits
+int digit_pins[4] = {2, 3, 4, 5};
+
+// Pins controlling the eight segments (8th is decimal point)
+int segment_pins[8] = {6, 7, 8, 9, 10, 11, 12, 13};
+
+// The four digit values to display
+int d1 = 0; int d2 = 0; int d3 = 0; int d4 = 0;
+
 
 void set_display(float value) {
     if (value > 9999) {
       return;
     }
+    // Cast value to int for now
+    int val = static_cast<int>(value);
 
-    d1 = -1; d2 = -1; d3 = -1
-    d4 = value % 10;
-
-    if (value >= 10) {
+    d1 = -1; d2 = -1; d3 = -1;
+    d4 = floor(val % 10);
+    if (val >= 10) {
       // Two digit number
-      d3 = floor((value % 100) / 10)
+      d3 = floor((val % 100) / 10);
     }
 
-    if (value >= 100) {
+    if (val >= 100) {
       // Three digit number
-      d2 = floor((value % 1000) / 100)
+      d2 = floor((val % 1000) / 100);
     }
     
-    if (value >= 1000) {
+    if (val >= 1000) {
       // Four digit number
-      d1 = floor(value / 1000);
+      d1 = floor(val / 1000);
     }
 
     // SET DIGITS
@@ -100,18 +153,4 @@ void set_all_low() {
     for (int i = 0; i < 8; i++) {
         digitalWrite(segment_pins[i], 0);
     }
-}
-
-void setup() {
-  init_all_pins();
-  // Serial.begin(9600);
-}
-
-void loop() {
-  unsigned long current_time = millis();
-  if (current_time - start_time > 500) {
-    temp += 1;
-    start_time = millis();
-  }
-  set_display(temp);
 }
